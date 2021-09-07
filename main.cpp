6e7a568d0219805ea51bed3e2dfe3fc839e2aa8f
Ranoiaetep <ranoiaetep@me.com>
2021-09-07 14:06:25 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Peter Cong on 9/6/21.
+//
+#include <iostream>
+#include "static_map.h"
+
+int main()
+{
+    auto s_m = static_map{{1,2},{3,4}};
+}
---
+++ static_map.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Peter Cong on 9/6/21.
+//
+
+#include "static_map.h"
