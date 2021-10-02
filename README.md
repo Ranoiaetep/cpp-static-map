a987e41ea7436e43dc29ce33168938f61b2b3ab1
Ranoiaetep <ranoiaetep@me.com>
2021-10-02 12:49:01 -0400

    Added documentation

--- README.md
+++ README.md
@@ -1,31 +1,49 @@
-# cpp-static-map/cpp-static-map
+# C++ Static Map
 
-
+Header-only compile time key-value map written in C++20.
 
 ## Getting Started
 
-Download links:
+Simply add the files in your source and `#include "@dir/Static_map.hpp"`, and replace `@dir` with the actual directory they are put in.
 
-SSH clone URL: ssh://git@git.jetbrains.space/ranoiaetep/cpp-static-map/cpp-static-map.git
+## Usage
 
-HTTPS clone URL: https://git.jetbrains.space/ranoiaetep/cpp-static-map/cpp-static-map.git
+### Creating a map
 
+To generate a `Static_map` containing data: `{1,"One"},{2,"Two"},{3,"Three}`:
 
+````c++
+constexpr auto map = make_static_map(1, "One"sv, 2, "Two"sv, 3, "Three"sv);
+````
 
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+To generate a `Static_map` where key and value are the same type, you can also do:
+
+```c++
+constexpr auto map = make_static_map({"1"sv, "One"sv}, {"2"sv, "Two"sv}, {"3"sv, "Three"sv});
+```
+
+-   Note: `sv` is the literal for `std::string_view`. Any type that can be constructed as `constexpr` should work.
+
+Alternatively, you can first create an `std::array<std::pair<Key, Value>, N>` object manually, and it can be explicitly converted to `Static_map<Key, Value, N>`.
+
+### Retrieving data
+
+There are three methods to retrieve value from the map with a given key:
+
+1.   `Static_map.at(KEY)` it returns the pointed value. If key was not found it will throws `std::out_of_range`.
+
+2.   `Static_map[KEY]` it returns an `std::optional<VALUE>`. If key was not found it will return an `std::nullopt`.
+3.   `Static_map.at_or_default(KEY, DEFAULT=VALUE{})` it returns the pointed value. If key was not found `DEFAULT` will be returned. If no default was given, the default constructed `VALUE` will be returned.
+
+-   Note: The second option will always returns an `std::optional`, whereas the other two will attempt to return the `VALUE` type directly.
+-   Note: The first method would fail in compile time if key was not found, as it requires exception handling.
+
+### Duplicated Key
+
+By default, `Static_map` will not check for duplicated keys. When retrieving data from a given key, only the first matched key will be considered.
+
+Optionally, you can pass a custom flag `SM_NO_DUPLICATE`, it will throw `std::invalid_argument` if duplicate `KEY` were presented.
 
 ## Prerequisites
 
-What things you need to install the software and how to install them.
-
-```
-Examples
-```
-
-## Deployment
-
-Add additional notes about how to deploy this on a production system.
-
-## Resources
-
-Add links to external resources for this project, such as CI server, bug tracker, etc.
+C++20 with range library support is required for now. C++14/17 support will be added later.
--- tests/make_static_map.cpp
+++ tests/make_static_map.cpp
@@ -1,7 +1,8 @@
 //
 // Created by Peter Cong on 10/2/21.
 //
-#include <cassert>
+
+#include <string_view>
 #include "Static_map.hpp"
 
 static_assert(make_static_map(1,2).size() == 1U);
@@ -9,11 +10,39 @@ static_assert(make_static_map(1,2,3,4,5,6).size() == 3U);
 static_assert(make_static_map(1,2,3,4,5,6).at(1) == 2);
 static_assert(make_static_map(1,2,3,4,5,6).at(3) == 4);
 static_assert(make_static_map(1,2,3,4,5,6)[1] == 2);
-static_assert(make_static_map(1,2,3,4,5,6).at_or_default(1, 0) == 2);
+static_assert(make_static_map(1,2,3,4,5,6)[2] == std::nullopt);
+static_assert(make_static_map(1,2,3,4,5,6).at_or_default(1, 1) == 2);
+static_assert(make_static_map(1,2,3,4,5,6).at_or_default(1) == 2);
+static_assert(make_static_map(1,2,3,4,5,6).at_or_default(2, 1) == 1);
+static_assert(make_static_map(1,2,3,4,5,6).at_or_default(2) == 0);
 static_assert(make_static_map({1,2},{3,4}).size() == 2U);
+static_assert(make_static_map({1,2},{3,4}).at(1) == 2);
 
-int main()
-{
-    assert(make_static_map(1,2,3,4,5,6)[2] == std::nullopt);
-    assert(make_static_map(1,2,3,4,5,6).at_or_default(2, 0) == 0);
-}
+using std::literals::string_view_literals::operator""sv;
+
+static_assert(make_static_map(1, "One"sv).size() == 1U);
+static_assert(make_static_map(1,"One"sv,2,"Two"sv,3,"Three"sv).size() == 3U);
+static_assert(make_static_map(1,"One"sv,2,"Two"sv,3,"Three"sv).at(1) == "One"sv);
+static_assert(make_static_map(1,2,3,4,5,6)[1] == 2);
+
+static_assert(make_static_map("One"sv, 1).size() == 1U);
+static_assert(make_static_map("One"sv,1,"Two"sv,2,"Three"sv,3).size() == 3U);
+static_assert(make_static_map("One"sv,1,"Two"sv,2,"Three"sv,3).at("One"sv) == 1);
+static_assert(make_static_map("One"sv,1,"Two"sv,2,"Three"sv,3).at("One") == 1);
+static_assert(make_static_map("One"sv,1,"Two"sv,2,"Three"sv,3)["One"] == 1);
+static_assert(make_static_map("One"sv,1,"Two"sv,2,"Three"sv,3)["Zero"] == std::nullopt);
+static_assert(make_static_map("One"sv,1,"Two"sv,2,"Three"sv,3).at_or_default("One", 1) == 1);
+static_assert(make_static_map("One"sv,1,"Two"sv,2,"Three"sv,3).at_or_default("Zero", 1) == 1);
+static_assert(make_static_map("One"sv,1,"Two"sv,2,"Three"sv,3).at_or_default("Zero") == 0);
+
+
+static_assert(make_static_map("1"sv, "One"sv).size() == 1U);
+static_assert(make_static_map("1"sv,"One"sv,"2"sv,"Two"sv,"3"sv,"Three"sv).size() == 3U);
+static_assert(make_static_map("1"sv,"One"sv,"2"sv,"Two"sv,"3"sv,"Three"sv).at("1"sv) == "One"sv);
+
+static_assert(make_static_map({"1"sv,"One"sv},{"2"sv,"Two"sv}).size() == 2U);
+static_assert(make_static_map({"1"sv,"One"sv},{"2"sv,"Two"sv}).at("1"sv) == "One"sv);
+static_assert(make_static_map({"1"sv,"One"sv},{"2"sv,"Two"sv}).at("1") == "One");
+static_assert(make_static_map({"1"sv,"One"sv},{"2"sv,"Two"sv}).at_or_default("1", "One") == "One");
+static_assert(make_static_map({"1"sv,"One"sv},{"2"sv,"Two"sv}).at_or_default("0", "One") == "One");
+static_assert(make_static_map({"1"sv,"One"sv},{"2"sv,"Two"sv}).at_or_default("0") == "");
